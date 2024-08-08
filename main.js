7a76a9f67dfeca4c1a2a82369f1bba2a97d128e9
teslacreator23 <135182581+teslacreator23@users.noreply.github.com>
2024-08-07 19:25:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function Math.floor().{
+    random_no = Math.floor((Math.random() *array_1.length) +1 )
+    Element_of_array = array_1[ random_no ]
+}
+console.log(){
+    quik_draw_data_set
+}
+
+
+timer_counter= 0
+
+timer_check =
+
+drawn_sketch =
+
+answer_holder =
+
+score = 0
